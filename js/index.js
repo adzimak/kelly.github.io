c43a0c9b353cdfdcaeab6ff14e1f4b6f6b338a4e
adzimak <adz13004@byui.edu>
2021-01-22 17:18:25 -0700

    initial

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-funtion todayDate(){
+function todayDate() {
     var now = new Date();
     var month = now.getMonth();
     var dayOfMonth = now.getDate();
     var dayOfWeek = now.getDay();
-    var hour = d.getHours();
-    var minute = d.getMinutes();
+    var hour = now.getHours();
+    var minutes = now.getMinutes();
 
     output = dayOfWeek + dayOfMonth + "@" + hour + ":" + minutes;
     document.getElementById("output").innerHTML = output;
